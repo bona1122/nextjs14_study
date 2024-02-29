987abf2749896ae41507fded1beff8b68a98f069
bona1122 <vnclfjqm12@gmail.com>
2024-02-29 23:52:02 +0900

    metadata

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,5 +1,11 @@
+export const metadata = {
+  title: "About us",
+};
+
 export default function AboutUs() {
-  return <div>
-    <h1>About Us</h1>
-  </div>
+  return (
+    <div>
+      <h1>About Us</h1>
+    </div>
+  );
 }
